db8f7358ca8f8ebaa41512a642ef795628ee70f3
sujay <jirapuresujay@gmail.com>
2023-01-24 23:18:36 +0530

    doneall

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import Header from "./Header";
 import Main from "./Main";
-import {Footer} from "./Footer";
+import Footer from "./Footer";
 function App() {
     
     return (
         <div className="container">
-            <header />
-            <main /> 
-            <footer />
+            <Header />
+            <Main /> 
+            <Footer />
         </div>
     );
 }
