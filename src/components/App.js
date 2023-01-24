41b23bfb2b8ccb0de686e5e15ebe08d0732bd597
sujay <jirapuresujay@gmail.com>
2023-01-24 23:48:31 +0530

    appdone

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ function App() {
     
     return (
         <div className="container">
+            
             <Header />
             <Main /> 
             <Footer />
--- src/components/Main.js
+++ src/components/Main.js
@@ -10,4 +10,4 @@ function Main(){
     );
 }
 
-export default Header;
+export default Main;
