9b941919a62f40f26cd33e485d52fafcfe98884b
sujay <jirapuresujay@gmail.com>
2023-01-24 23:05:20 +0530

    donefooter7

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Header from "./Header";
 import Main from "./Main";
-import Footer from "./Footer";
+import {Footer} from "./Footer";
 function App() {
+    
     return (
         <div className="container">
-            <header id="my-header">This is the header</header>
-            <main id="my-main">This is the main</main>
+            <header />
+            <main /> 
             <footer />
         </div>
     );
