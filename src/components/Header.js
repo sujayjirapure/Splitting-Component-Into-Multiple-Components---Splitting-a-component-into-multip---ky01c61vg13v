f9d3890892e27e99bd0fef553e22f20d2a233675
sujay <jirapuresujay@gmail.com>
2023-01-24 23:56:13 +0530

    appdone2

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,9 @@ import React from "react"
 function Header(){
     return (
         <div id="header">
-            <header id="my-header">
+            
                 This is the header ok
-            </header>
+            
         </div>
     );
 }
