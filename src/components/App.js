73455bc1b462b8fec6437b1a559e76c2cc5e0c5a
Sujay jirapure <110679753+sujayjirapure@users.noreply.github.com>
2023-01-25 14:05:47 +0530

    9th

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
+
 import React from "react";
 import Header from "./Header";
 import Main from "./Main";
 import Footer from "./Footer";
 function App() {
-    
     return (
-        <div className="container">
+        <div id="container">
             <Header id="my-header" content="This is the header"/>
             <Main id="my-main" content="This is the main"/>
             <Footer id="my-footer" content="This is the footer"/>
