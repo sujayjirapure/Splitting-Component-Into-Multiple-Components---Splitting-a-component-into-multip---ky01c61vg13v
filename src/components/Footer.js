a869d07dea38715f327ff1dc214f07eb5c8fde1c
Sujay jirapure <110679753+sujayjirapure@users.noreply.github.com>
2023-01-25 14:03:03 +0530

    8th

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 function Footer(props){
-    let {id ,content} = props;
+    const {id , content} = props;
     return (
         <div id="footer">
             <footer id={id}>{content}</footer>
